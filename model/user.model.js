c6bb94ad69c435dbacfd872881c60b0e5f21b240
Etienne Belde <ejm.belde@student.avans.nl>
2017-11-21 14:39:37 +0100

    test

--- model/user.model.js
+++ model/user.model.js
@@ -2,7 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: String
+    name: String,
+    title: String,
+    recipes: [{
+        name: String,
+        ingredients: [{
+            name: String,
+            weight: Number,
+            price: String
+        }]
+    }]
+
+  
+
 }, {
     timestamps: true
 });
@@ -13,6 +25,20 @@ const User = mongoose.model('user', UserSchema);
 // Add a 'dummy' user (every time you require this file!)
 const user = new User({
     name: 'Joe',
-}).save();
+    title: 'Mr.',
+    recipes: [
+        {name: 'pasta bolognaise', ingredients: [
+        {name: 'Tomaten', weight: 200, price:'1,20'}   ]
+        },
+        {
+            name: 'spaghetti carbonara',
+            ingredients:[]
+        },
+        {
+            name: 'pizza'
+        }
+    ]
+
+});//.save();
 
 module.exports = User;
